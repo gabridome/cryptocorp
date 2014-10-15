0bea8cf56722f7ac5b7091364940e7c7fcaf1edf
Gabriele Domenichini <gabridome@fastmail.fm>
2014-10-15 16:47:38 +0200

    primo commit

---
+++ cryptocorp.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+#
+# Please don't forget to customize the "contacts" variable with your own references
+#
+#
+# syntax: contacts = [{"email":"email1@somedomain.com"},{"email":"email2@somedomain.com"},{"sms":"+12345678910"},{"web":"https://somecall-back.com"}]
+
