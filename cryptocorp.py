b89015e07d7d9d0b0d2e4c104d895ab5e0a5d9d7
Gabriele Domenichini <gabridome@fastmail.fm>
2014-10-18 19:17:03 +0200

    Sistemato fino alla prova in sandbox.
    Qualcosa non gli piace. Secondo me:
    O i parametri non vanno bene oppure hanno problemi col server.

--- cryptocorp.py
+++ cryptocorp.py
@@ -1,33 +1,30 @@
 #!/usr/bin/python
+#Prima di tutto devo generare 128, 256 o 512 bits di entropia. Consigliati 256.
+# Questo é il seed.
+# Da questo con uno SHA 512 ottengo una stringa di 64 caratteri (512 bits)
+# Divido la stringa in due da 128 bits (o 32 caratteri e con i primi 32 facciola Master private key e con la seconda faccio il chain code)
+# Con la chiave privata ottengo una chiave pubblica.
 
-#extended public keys
+# Con queste due e il chain code ottengo la prima coppia (pubblica e privata di extended keys (k,c) privata e (K,c) pubblica)
+# Questo é il master extendeded node.
+# Nella notazione ufficiale é chiamato m
+# La struttura di defaul prevede che i figli siano gli accounts quindi possono essere innumerevoli.
+# Ciascuno degli accounts però può avere solo due figli: 0 e 1 rispettivamente per gli indirizzi esterni e interni.
+# I figli esterni ed interni possono poi avere innumerevoli figli e nipoti. Es: primo account (0), esterno (0) portafoglio 532 =m/0/0/532
 
-# preparo i New keychain parameters:
-#
-# Please don't forget to customize the "contacts" variable with your own references
-#
-#
-# syntax: contacts = [{"email":"email1@somedomain.com"},{"email":"email2@somedomain.com"},{"sms":"+12345678910"},{"web":"https://somecall-back.com"}]
-# The key tree
 
-#The next step is cascading several CKD constructions to build a tree. 
-# We start with one root, the master extended key m. 
-# By evaluating CKDpriv(m,i) for several values of i, 
-# we get a number of level-1 derived nodes. 
-# As each of these is again an extended key, CKDpriv can be applied to those as well.
+#To shorten notation, we will write CKDpriv(CKDpriv(CKDpriv(m,3H),2),5) as m/3H/2/5. Equivalently for public keys, we write CKDpub(CKDp
+# Master key generation
 
-#To shorten notation, we will write CKDpriv(CKDpriv(CKDpriv(m,3H),2),5) as m/3H/2/5. Equivalently for public keys, we write CKDpub(CKDpub(CKDpub(M,3),2,5) as M/3/2/5. This results in the following identities:
+# Generate a ********* seed *********** byte sequence S of a chosen length (between 128 and 512 bits; 256 bits is advised) from a (P)RNG.
+# Calculate I = HMAC-SHA512(Key = "Bitcoin seed", Data = S)
+# Split I into two 32-byte sequences, IL and IR.
+# Use parse256(IL) as master secret key, and IR as master chain code.
 
-#N(m/a/b/c) = N(m/a/b)/c = N(m/a)/b/c = N(m)/a/b/c = M/a/b/c.
-#N(m/aH/b/c) = N(m/aH/b)/c = N(m/aH)/b/c.
-#However, N(m/aH) cannot be rewritten as N(m)/aH, as the latter is not possible.
-#Each leaf node in the tree corresponds to an actual key, while the internal nodes correspond to the collections of keys that descend from them. The chain codes of the leaf nodes are ignored, and only their embedded private or public key is relevant. Because of this construction, knowing an extended private key allows reconstruction of all descendant private keys and public keys, and knowing an extended public keys allows reconstruction of all descendant non-hardened public keys.
-
-#  m = Master extended key (Pieter Wuille)
 
 ############################ uno
-# bip32 extended  key = m = the "seed" = master extended key
-bip32xk = "xprv9s21ZrQH143K3YWKyCt7zyDZdbWHRLxT8u66rUg1nCKgVShH3ELECXpnsaCgSMfRv3GpxT4cLH8Xy1YvPqYJDF2QagYya8oa7Hb7YQgyZgN"
+# bip32 extended  key = m = master extended key
+msk1 = "xprv9s21ZrQH143K3YWKyCt7zyDZdbWHRLxT8u66rUg1nCKgVShH3ELECXpnsaCgSMfRv3GpxT4cLH8Xy1YvPqYJDF2QagYya8oa7Hb7YQgyZgN"
 # Bitcoin Master Private Key
 # Version 0488ade4
 # Depth 0
@@ -40,41 +37,50 @@ bip32xk = "xprv9s21ZrQH143K3YWKyCt7zyDZdbWHRLxT8u66rUg1nCKgVShH3ELECXpnsaCgSMfRv
 # impostando a "m" per master si ottiene
 # Private key (WIF) KzpRD7AseqCJsxHGadoHwLYVPPXBBSRPcpSM9Da6Gum1Q2FmXN2m
 # Derived Public Key (in realtà é una master) 
-dpk = "xpub661MyMwAqRbcG2ao5ER8N7AJBdLmpogJW81hes5dLXrfNF2RameUkL9GisfAt3CeqoE7oY8RdKwHKcYAZUb4MtZhnPi4ABRP185iYCrrxwT"
+mpk1 = "xpub661MyMwAqRbcG2ao5ER8N7AJBdLmpogJW81hes5dLXrfNF2RameUkL9GisfAt3CeqoE7oY8RdKwHKcYAZUb4MtZhnPi4ABRP185iYCrrxwT"
 # Questi non verranno usati perché fanno parte di una master
+msk2 = "xprv9s21ZrQH143K3TBwt5FDxr4Hse7HpxyBMiEvRa3RHKrWCqWZ9dWVfTzpirNWjYWiQQuQzYWAQsXD4PbooL3dv6wBNVEjkVH83FRFeMJCk14"
+mpk2 = "xpub661MyMwAqRbcFwGQz6nEKz12RfwnERh2iwAXDxT2qfPV5dqhhApkDGKJa9hrNJXEnCjR6aXmuPrAanjeSSh6Qpo2sFCpVqDucNxon7z4un7"
+
+msk3 = "xprv9s21ZrQH143K2wgWS7zXaxneoVibFbfpvtGRKTpWNfzv6uT8TeLR9mRaf1Heez31SwdqVg2EjK1C21JSve4iWjBkjSxoLY11GZX4nh1P4rq"
+mpk3 = "xpub661MyMwAqRbcFRkyY9XXx6jPMXZ5f4PgJ7C27rE7w1XtyhnH1BefhZk4WH23KEcMxvJyote6TX5sGd1HpEvy3fB66fmhXWjFURatYZcP8sw"
+
+
+
+
 
 #Public key (Hex) # Private key (WIF) KzpRD7AseqCJsxHGadoHwLYVPPXBBSRPcpSM9Da6Gum1Q2FmXN2m
 # address 1HTTvgjWv9zdYdhCvu9biRG3gYu2RBVhCq
 
+import pycoin
+import uuid
 
-keychainId = str(uuid.uuid5(uuid.NAMESPACE_URL, "urn:digitaloracle.co:%s"%(dpk)))
+
+keychainId = str(uuid.uuid5(uuid.NAMESPACE_URL, "urn:digitaloracle.co:%s"%(mpk3)))
 
 #Ora che hai le master keys devi costruire le chiavi figlie
+
 
 payload =  {
     "walletAgent": "HDM-2.0-cc-100",
     "rulesetId": "default",
-    "keys": [ epk1,epk2,epk3 ],
+    "keys": [mpk1, mpk2, mpk3],
     "parameters": {
-        "velocity_1": {
-            "value": 0.8,
+        "levels": [ {
             "asset": "BTC",
-            "period": "86400",
-            "limited_keys": ["3"]
-        },
-        "delay_2": 600,
-        "call_2": ["phone"], 
-        "verify_2": ["otp"],
-        "otp_type": "totp",
-        "otp_secret": "IS3TZZIPTDPM2YGT4CDXEOFK",
-        "otp": "123456"
-    },
-   "pii": {
-        "email": "gabridome@fastmail.fm",
-        "phone": "+393489992529",
-    }
+            "period": 60,
+            "value": 0.0
+        }, {
+            "delay": 0,
+            "calls": ['phone', 'email']
+        }, ]
+   },
+    "pii": { 
+        "email": "user@example.com", 
+        "phone": "+14155551212" 
+    } 
 }
-apiurl = "http://s.digitaloracle.co/"
+apiurl = "https://s.digitaloracle.co"
 command = "keychains"
 requrl = apiurl + "/" + command + "/" + keychainId    
 r = requests.post(requrl, data=payload)
