caf4b2205791578c0bb04862deb9389079eddab0
Gabriele Domenichini <gabridome@fastmail.fm>
2014-11-25 17:44:02 +0100

    switched to pybitcointools

---
+++ snippets/build_address.py
@@ -0,0 +1,21 @@
+# take the results and build the p2sh address and script.
+import json
+import ast
+import bitcoin
+from bitcoin.deterministic import *
+
+# Scrive un file.
+data = open("results.json")
+json_data = json.load(data)
+json_data = ast.literal_eval(json.dumps(json_data))
+private_wallets = json_data['private_wallets']
+public_wallets = json_data['public_wallets']
+#costruisco un figlio
+publics = [bip32_ckd(key,5) for key in public_wallets] #it works
+privates = [bip32_ckd(key,5) for key in private_wallets]
+# from which the public keys
+public_keys = [bip32_extract_key(key) for key in publics]
+private_keys = [bip32_extract_key(key) for key in privates]
+
+script = mk_multisig_script(public_keys,2,3)
+address = scriptaddr(script)
