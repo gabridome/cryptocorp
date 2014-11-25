48e3d18578aaba1f158e24f15bbc06e53fec0f36
Gabriele Domenichini <gabridome@fastmail.fm>
2014-11-25 23:45:13 +0100

    path finished without support for hardened keys

--- snippets/build_address.py
+++ snippets/build_address.py
@@ -3,6 +3,7 @@ import json
 import ast
 import bitcoin
 from bitcoin.deterministic import *
+from bitcoin.transaction import *
 
 # Scrive un file.
 data = open("results.json")
@@ -11,17 +12,30 @@ json_data = ast.literal_eval(json.dumps(json_data))
 private_wallets = json_data['private_wallets']
 public_wallets = json_data['public_wallets']
 #costruisco un figlio
-publics = [bip32_ckd(key,5) for key in public_wallets] #it works
-privates = [bip32_ckd(key,5) for key in private_wallets]
+#private_wallets = [bip32_ckd(key,5) for key in private_wallets]
+#public_wallets = [bip32_ckd(key,5) for key in public_wallets] #it works
 
 # path
 path = '0/0/7'
 listfrompath = path.split('/')
-pathinnum = [int(n) for n in (path.split('/'))]
+pathinnum = [int(n) for n in (path.split('/'))] # hardened not supported
 
-# from which the public keys
-public_keys = [bip32_extract_key(key) for key in publics]
-private_keys = [bip32_extract_key(key) for key in privates]
+
+private_subwallets = sub_wallet(pathinnum,private_wallets)
+public_subwallets = sub_wallet(pathinnum,public_wallets)
+
+# from which the secret keys
+private_keys = [bip32_extract_key(key) for key in private_subwallets]
+public_keys = [bip32_extract_key(key) for key in public_subwallets]
 
 script = mk_multisig_script(public_keys,2,3)
 address = scriptaddr(script)
+
+print(script)
+print(address)
+print()
+def sub_wallet(path,wallets):
+	subwallets = wallets
+	for i in path:
+		subwallets = [bip32_ckd(key,i) for key in subwallets]
+	return subwallets
