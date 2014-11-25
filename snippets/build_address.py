1504887f5c74d037eb34d711305d80f8d9afa9c8
Gabriele Domenichini <gabridome@fastmail.fm>
2014-11-26 00:12:26 +0100

    pretty print :)

--- snippets/build_address.py
+++ snippets/build_address.py
@@ -10,7 +10,15 @@ data = open("results.json")
 json_data = json.load(data)
 json_data = ast.literal_eval(json.dumps(json_data))
 private_wallets = json_data['private_wallets']
+print("")
+print("Private BIP32 extended wallets")
+for i in private_wallets: print(i)
+
+print("")
 public_wallets = json_data['public_wallets']
+print("Public BIP32 extended wallets")
+for i in public_wallets: print(i)
+print("")
 #private_wallets = [bip32_ckd(key,5) for key in private_wallets]
 #public_wallets = [bip32_ckd(key,5) for key in public_wallets] #it works
 def sub_wallet(path,wallets):
@@ -26,15 +34,32 @@ pathinnum = [int(n) for n in (path.split('/'))] # hardened not supported
 
 
 private_subwallets = sub_wallet(pathinnum,private_wallets)
+print("")
+print("private subwallets with path %s" % path)
+for i in private_subwallets: print(i)
+
+
 public_subwallets = sub_wallet(pathinnum,public_wallets)
+print("")
+print("public subwallets with path %s" % path)
+for i in public_subwallets: print(i)
 
 # from which the secret keys
 private_keys = [bip32_extract_key(key) for key in private_subwallets]
+print("")
+print("private keys with path %s" % path)
+for i in private_keys: print(i)
+
 public_keys = [bip32_extract_key(key) for key in public_subwallets]
+print("")
+print("Public keys with path %s" % path)
+for i in public_keys: print(i)
 
 script = mk_multisig_script(public_keys,2,3)
 address = scriptaddr(script)
-
+print("")
+print("script 2 of 3:")
 print(script)
-print(address)
-print()
+print("")
+print("related address: %s" % address)
+print("")
